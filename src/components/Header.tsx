6f50d042b5c487c77ef675ac01472821181b1193
Azmir <azmirhasan.az2@gmail.com>
2023-11-04 00:32:10 +0600

    FIX

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,24 +8,29 @@ const Header = ({ total, handleDeleteClick, clearSelect }: Props) => {
   return (
     <div className='p-3 border-b text-black font-semibold'>
       <div className='flex justify-between items-center'>
-        <div className='flex gap-4'>
-          <div className='font-semibold'>{total} Files Selected</div>
+        <div>
+          {total === 0 && <div className='font-bold'>Gallery</div>}
           {total > 0 && (
-            <button
-              className='border border-green-500 text-xs px-1 rounded hover:bg-green-600 hover:text-white transition-all duration-150'
-              onClick={clearSelect}
-            >
-              Reset
-            </button>
+            <div className='flex gap-4'>
+              <div className='font-semibold'>{total} Files Selected</div>
+              <button
+                className='border border-green-500 text-xs px-1 rounded hover:bg-green-600 hover:text-white transition-all duration-150'
+                onClick={clearSelect}
+              >
+                Reset
+              </button>
+            </div>
           )}
         </div>
         <div>
-          <button
-            onClick={handleDeleteClick}
-            className='text-red-500 font-semibold'
-          >
-            Delete Files
-          </button>
+          {total > 0 && (
+            <button
+              onClick={handleDeleteClick}
+              className='text-red-500 font-semibold'
+            >
+              Delete Files
+            </button>
+          )}
         </div>
       </div>
     </div>
